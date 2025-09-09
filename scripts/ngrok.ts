be202e06d1941da3ed7d3edb74c5415cbd63c9aa
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2025-09-09 17:51:10 +0000

    ngroc start and unsafe user data

---
+++ scripts/ngrok.ts
@@ -0,0 +1,13 @@
+import ngrok from "ngrok";
+import dotenv from "dotenv";
+dotenv.config();
+(async function () {
+  const token = process.env.NGROK_TOKEN;
+  if (!token) {
+    console.log("NGROK_TOKEN miss");
+    return;
+  }
+  await ngrok.authtoken(token);
+  const url = await ngrok.connect(3000);
+  console.log(url);
+})();
