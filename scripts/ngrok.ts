f928aef2a9a225879481964cc6f464613d79e677
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2025-09-09 17:09:58 +0000

    cleane

--- scripts/ngrok.ts
+++
@@ -1,6 +0,0 @@
-import ngrok from "ngrok"
-( async  function ( )  { 
-    await ngrok.authtoken("32Pa80lmOvyUCyIDRGzuXZD3um1_67AFDvFsCbpnFBnb7bz1S")
-  const  url  =  await  ngrok . connect (3000) ; 
-  console.log(url)
-  } ) ( ) ;
