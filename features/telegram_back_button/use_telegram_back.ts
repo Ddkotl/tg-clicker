4a8b904b696748cf81713a71dab0ccf933b9f58a
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2025-10-21 08:01:40 +0300

    Update use_telegram_back.ts

--- features/telegram_back_button/use_telegram_back.ts
+++ features/telegram_back_button/use_telegram_back.ts
@@ -5,7 +5,13 @@ import { useRouter, usePathname } from "next/navigation";
 export function useTelegramBack() {
   const router = useRouter();
   const pathname = usePathname();
-
+const handleBack = () => {
+        try {
+          router.back()
+        } catch (err) {
+          console.error("Telegram back handler error:", err);
+        }
+      };
   useEffect(() => {
     import("@twa-dev/sdk").then(({ default: WebApp }) => {
       const hideBackPaths = ["/", "/registration"];
@@ -17,29 +23,21 @@ export function useTelegramBack() {
 
       WebApp.BackButton.show();
 
-      const handleBack = () => {
-        try {
-          router.push("/game");
-        } catch (err) {
-          console.error("Telegram back handler error:", err);
-        }
-      };
-
       // ✅ важно: Telegram сам вызывает handleBack — не нужно давать ему закрывать WebApp
       WebApp.BackButton.onClick(() => {
         handleBack();
       });
 
       // обрабатываем системную кнопку Android
-      const handleSystemBack = (event: PopStateEvent) => {
-        event.preventDefault();
-        handleBack();
-      };
-      window.addEventListener("popstate", handleSystemBack);
+      //const handleSystemBack = (event: PopStateEvent) => {
+     //   event.preventDefault();
+       // handleBack();
+   //   };
+    //  window.addEventListener("popstate", handleSystemBack);
 
       return () => {
         WebApp.BackButton.offClick(handleBack);
-        window.removeEventListener("popstate", handleSystemBack);
+     //   window.removeEventListener("popstate", handleSystemBack);
       };
     });
   }, [router, pathname]);
