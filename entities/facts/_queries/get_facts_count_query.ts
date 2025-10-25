0585db5cfd63355a47bb0b10f9dd1b604207a605
Ddtester5 <dd5892631@gmail.com>
2025-10-25 13:13:20 +0000

    fix fact count

--- entities/facts/_queries/get_facts_count_query.ts
+++ entities/facts/_queries/get_facts_count_query.ts
@@ -1,9 +1,10 @@
+import { api_path } from "@/shared/lib/paths";
 import { queries_keys } from "@/shared/lib/queries_keys";
 
 export const getFactCountNocheckQuery = (userId: string) => ({
   queryKey: queries_keys.facts_userId(userId),
   queryFn: async ({ signal }: { signal: AbortSignal }) => {
-    const res = await fetch(`/api/user/count_nocheck${userId ? `?userId=${userId}` : ""}`, { signal });
+    const res = await fetch(api_path.get_facts_count_nocheck(userId), { signal });
     if (!res.ok) {
       throw new Error("Failed to fetch count_nocheck facts");
     }
--- entities/facts/_repositories/get_facts_nocheck_count.ts
+++ entities/facts/_repositories/get_facts_nocheck_count.ts
@@ -2,7 +2,7 @@ import { dataBase } from "@/shared/connect/db_connect";
 export async function getFactNocheckCount(userId: string) {
   try {
     const count = await dataBase.facts.count({
-      where: { userId },
+      where: { userId: userId, status: "NO_CHECKED" },
     });
     return count;
   } catch (error) {
