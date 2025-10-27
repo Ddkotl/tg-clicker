2e031509111a8f82bc5b6d060c5b2e2aaaf07d47
qweq05293 <qweq05293@gmail.com>
2025-10-27 11:38:41 +0300

    Update icons.tsx

--- shared/lib/icons.tsx
+++ shared/lib/icons.tsx
@@ -1,5 +1,12 @@
-import { Droplet } from "lucide-react";
+import { GiFire } from "react-icons/gi";
 import { ReactNode } from "react";
+import { MdOutlineTempleHindu } from "react-icons/md";
+import { GiHadesSymbol } from "react-icons/gi";
+import { GiBloodySword } from "react-icons/gi";
+import { GiHolyGrail } from "react-icons/gi";
+import { GiStonePile } from "react-icons/gi";
+import { GiDigDug } from "react-icons/gi";
+import { GiDodging } from "react-icons/gi";
 
 type IconProps = {
   size?: number;
@@ -9,11 +16,42 @@ type IconProps = {
 
 export const icons = {
   qi_energy: ({ size = 20, color = "blue", className = "" }: IconProps): ReactNode => (
-    <Droplet size={size} color={color} className={className} />
+    <GiFire size={size} color={color} className={className} />
   ),
   exp: ({ size = 20, color = "green", className = "" }: IconProps): ReactNode => (
-    <svg viewBox="0 0 24 24" fill={color} width={size} height={size} className={className}>
-      <polygon points="12,2 15,8 21,12 15,16 12,22 9,16 3,12 9,8" />
-    </svg>
+    <GiDodging size={size} color={color} className={className} />
+  ),
+  stone: ({ size = 20, color = "yellow", className = "" }: IconProps): ReactNode => (
+    <GiStonePile size={size} color={color} className={className} />
+  ),
+  crystal: ({ size = 20, color = "purple", className = "" }: IconProps): ReactNode => (
+    <GiFloatingCrystal size={size} color={color} className={className} />
+  ),
+  notification: ({ size = 20, color = "primary", className = "" }: IconProps): ReactNode => (
+    <BellDot /> size={size} color={color} className={className} />
+  ),
+  fact: ({ size = 20, color = "white", className = "" }: IconProps): ReactNode => (
+    <ScrollText size={size} color={color} className={className} />
+  ),
+  temple: ({ size = 20, color = "white", className = "" }: IconProps): ReactNode => (
+    <MdOutlineTempleHindu size={size} color={color} className={className} />
+  ),
+  citadel: ({ size = 20, color = "white", className = "" }: IconProps): ReactNode => (
+    <GiHadesSymbol size={size} color={color} className={className} />
+  ),
+  fight: ({ size = 20, color = "white", className = "" }: IconProps): ReactNode => (
+    <GiBloodySword size={size} color={color} className={className} />
+  ),
+  pet: ({ size = 20, color = "white", className = "" }: IconProps): ReactNode => (
+    <PawPrint size={size} color={color} className={className} />
+  ),
+  trophy: ({ size = 20, color = "white", className = "" }: IconProps): ReactNode => (
+    <GiHolyGrail  size={size} color={color} className={className} />
+  ),
+  dig: ({ size = 20, color = "white", className = "" }: IconProps): ReactNode => (
+    <GiDigDug size={size} color={color} className={className} />
+  ),
+  meditation: ({ size = 20, color = "white", className = "" }: IconProps): ReactNode => (
+    <Hourglass size={size} color={color} className={className} />
   ),
 };
