f4f9c5fbb9f8222c6c6b9fef426cf67ac1013615
qweq05293 <qweq05293@gmail.com>
2025-11-04 11:03:09 +0300

    Enhance translation function to handle ReactNode
    Updated the translation function to support ReactNode for variables.

--- features/translations/use_translation.ts
+++ features/translations/use_translation.ts
@@ -1,15 +1,14 @@
 "use client";
+import { ReactNode, useMemo } from "react";
 import { en_lang } from "@/features/translations/langs/en";
 import { ru_lang } from "@/features/translations/langs/ru";
 import { useLanguage } from "@/features/translations/lang_context";
-import { useMemo } from "react";
 import { TranslationKey } from "./translate_type";
 
 const messages = { en: en_lang, ru: ru_lang };
 
 export function useTranslation() {
   const { language } = useLanguage();
-
   const dict = useMemo(() => messages[language], [language]);
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -17,12 +16,25 @@ export function useTranslation() {
     return path.split(".").reduce((acc, part) => acc?.[part], obj) ?? path;
   }
 
-  function t(key: TranslationKey, vars?: Record<string, string | number>) {
+  function t(
+    key: TranslationKey,
+    vars?: Record<string, string | number | ReactNode>
+  ): ReactNode {
     const raw = getValue(dict, key);
     if (!vars) return raw;
 
-    return raw.replace(/\{(\w+)\}/g, (_, name) => String(vars[name] ?? ""));
+    const parts = raw.split(/(\{.*?\})/g); // разбиваем по {var}
+    return parts.map((part, i) => {
+      const match = part.match(/^\{(.*)\}$/);
+      if (match) {
+        const name = match[1];
+        const value = vars[name];
+        return <span key={i}>{value}</span>;
+      }
+      return <span key={i}>{part}</span>;
+    });
   }
 
   return { t, language };
 }
+
