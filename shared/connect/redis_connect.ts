2bbc3b9e22843de9655562f1c1105e87d276b49d
Ddtester5 <dd5892631@gmail.com>
2025-12-01 16:19:28 +0000

    fix redis master

--- shared/connect/redis_connect.ts
+++ shared/connect/redis_connect.ts
@@ -3,6 +3,7 @@ import Redis from "ioredis";
 export const redis_conf = {
   host: "localhost",
   port: 6379,
+  password: process.env.REDIS_PASSWORD!,
 };
 
 export const redis = new Redis(redis_conf);
