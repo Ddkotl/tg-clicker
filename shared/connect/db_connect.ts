a9ff51da2a3b6f1058d3cb34297e60878dc0da76
Ddtester5 <dd5892631@gmail.com>
2025-12-06 14:40:34 +0000

    fix:migrate to prisma 7

--- shared/connect/db_connect.ts
+++ shared/connect/db_connect.ts
@@ -1,10 +1,15 @@
-import { Prisma, PrismaClient } from "@/_generated/prisma";
+import { Prisma, PrismaClient } from "@/_generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
 const globalForPrisma = global as unknown as {
   prisma?: PrismaClient;
 };
 
-export const dataBase = globalForPrisma.prisma ?? new PrismaClient();
+export const dataBase = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = dataBase;
 
