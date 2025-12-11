eb7e39f7274c15b6b32816213e723d23a3cfe036
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2025-12-11 13:10:16 +0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,20 +4,20 @@
 
 echo "=== DEPLOY START: $(date) ==="
 
-cd /home/admin/www/tg-clicker
+#cd /home/admin/www/tg-clicker
 #export NVM_DIR="$HOME/.nvm"
 #[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-echo "Using node version:"
+#echo "Using node version:"
 ##nvm use 24.11.1
 
-echo "Running Prisma migrate..."
+#echo "Running Prisma migrate..."
 #npx prisma migrate deploy
 
-echo "Generating Prisma client..."
+#echo "Generating Prisma client..."
 #npx prisma generate
 
-echo "Restarting PM2..."
+#echo "Restarting PM2..."
 #pm2 restart all
 
 echo "=== DEPLOY FINISH: $(date) ==="
